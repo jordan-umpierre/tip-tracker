1035266dafeac77f70177d23bc033f8759486f9b
Jordan Umpierre <jordanumpierre@gmail.com>
2026-08-03 14:17:29 -0500

    refactor: remove unused LogScreen import

--- src/screens/LogScreen.tsx
+++ src/screens/LogScreen.tsx
@@ -1,7 +1,7 @@
 import { useFocusEffect } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useCallback, useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CreateJobForm from '../components/CreateJobForm';
 import LogShiftForm from '../components/LogShiftForm';
